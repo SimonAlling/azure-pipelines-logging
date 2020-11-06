9508451ad013a0233754048224276c4a2527a0fc
Simon Alling <SimonAlling@users.noreply.github.com>
2020-11-06 21:45:45 +0100

    Use multiline style for types in format.ts (#3)

--- src/internal/format.ts
+++ src/internal/format.ts
@@ -1,6 +1,17 @@
-export type FormatWithMessage = "command" | "debug" | "error" | "group" | "warning";
+export type FormatWithMessage =
+    | "command"
+    | "debug"
+    | "error"
+    | "group"
+    | "warning"
+    ;
 
-export type FormatWithoutMessage = "endgroup";
+export type FormatWithoutMessage =
+    | "endgroup"
+    ;
 
 // https://docs.microsoft.com/en-us/azure/devops/pipelines/scripts/logging-commands#formatting-commands
-export type Format = FormatWithMessage | FormatWithoutMessage;
+export type Format =
+    | FormatWithMessage
+    | FormatWithoutMessage
+    ;
